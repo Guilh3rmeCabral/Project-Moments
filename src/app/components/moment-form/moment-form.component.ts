2c424f58008afc10932063a458024603fc6f67fc
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-13 20:30:48 -0300

    Feat: Definindo a validação do formulário

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,4 +9,32 @@ import { Component, Input } from '@angular/core';
 export class MomentFormComponent {
 
   @Input() btnText!: string;
+  
+  momentForm!: FormGroup;
+  
+  constructor () {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+
+  submit() {
+    if(this.momentForm.invalid) {
+      return;
+    }
+  }
 }
