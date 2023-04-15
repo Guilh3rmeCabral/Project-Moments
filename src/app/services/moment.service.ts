a938323ca607c640acfde0b46c6f475c2ec0d430
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-15 20:53:01 -0300

    Feat: Inserindo dados no sistema

---
+++ src/app/services/moment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MomentService {
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+
+  constructor(private http: HttpClient) { }
+
+  createMoment(formData: FormData): Observable<FormData> {
+    return this.http.post<FormData>(this.apiUrl, formData)
+  }
+}
