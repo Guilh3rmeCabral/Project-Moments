5f9e1440c06dec9bbddb596d6dbf7ac61d9f26fa
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-15 20:52:36 -0300

    Feat: Inserindo dados no sistema

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,6 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class MomentFormComponent {
 
+  @Output() onSubmit = new EventEmitter<Moment>
   @Input() btnText!: string;
   
   momentForm!: FormGroup;
@@ -31,10 +33,16 @@ export class MomentFormComponent {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    this.momentForm.patchValue({image: file})
+  }
 
   submit() {
     if(this.momentForm.invalid) {
       return;
     }
+
+    this.onSubmit.emit(this.momentForm.value);
   }
 }
