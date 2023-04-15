ac0f359429a0d873bbc88ed5f66280f63de2451e
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-15 20:52:45 -0300

    Feat: Inserindo dados no sistema

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -7,5 +9,31 @@ import { Component } from '@angular/core';
 })
 export class NewMomentComponent {
   btnText = 'Share!'
+
+  constructor (
+    private momentService: MomentService
+  ) {}
+
+  async creatHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+
+    if (moment.image) {
+      formData.append("image", moment.image);
+    }
+
+    //todo
+
+
+    //enviar para o service
+    await this.momentService.createMoment(formData).subscribe();
+
+    // Exibir msg
+
+
+    //redirect
+  }
 
 }
