2f80495a13d256022e839143456a93fea13b7cd6
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-24 18:45:56 -0300

    Feat: Atribuindo dados na variavel e inicializando com formatação de data

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/app/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
+  constructor (
+    private momentService: MomentService,
+
+  ) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items)=> {
+      const data = items.data
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
 }
