05e73e5d07681cf481f0d3582ce72e0ae1602f80
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-25 20:07:39 -0300

    Feat: Retornando o id e exibindo

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-moment',
   templateUrl: './moment.component.html',
@@ -7,4 +11,18 @@ import { Component } from '@angular/core';
 })
 export class MomentComponent {
 
+  moment?: Moment;
+
+  constructor (
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+
+    this.momentService
+      .getMoment(id)
+      .subscribe((item => this.moment = item.data))
+  }
 }
