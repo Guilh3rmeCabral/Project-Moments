a8389fb2221c133eb1918e51c3ff5bcf38899742
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-28 20:49:19 -0300

    Feat: Dados aplicados para o envio das informações ao service

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment';
+import { MessagesService } from 'src/app/services/messages.service';
 import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
@@ -16,7 +17,9 @@ export class EditMomentComponent {
 
   constructor (
     private momentService: MomentService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messagesService: MessagesService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -25,6 +28,22 @@ export class EditMomentComponent {
     this.momentService.getMoment(id).subscribe((item) => {
       this.moment = item.data;
     })
+  }
+
+  async editHandler(momentData : Moment) {
+    const id = this.moment.id
+    const formData = new FormData()
+
+    formData.append('title', momentData.title)
+    formData.append('description', momentData.description)
+
+    if (momentData.image) formData.append('image', momentData.image);
+
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messagesService.add(`Moment ${id} was updated with success!`);
+
+    this.router.navigate(['/']);
   }
 
 }
