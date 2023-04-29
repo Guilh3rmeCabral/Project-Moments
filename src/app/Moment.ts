5c704cdfb40c5bd25bad3ef5976ff5ab7b3041cc
Guilherme Cabral <guicabralprowork@gmail.com>
2023-04-29 19:04:25 -0300

    Refactor: concertando nome do dado

--- src/app/Moment.ts
+++ src/app/Moment.ts
@@ -5,5 +5,5 @@ export interface Moment {
     image: string;
     created_at?: string;
     updated_at?: string;
-    coments?: [{text: string, username: string}];
+    comments?: [{text: string, username: string}];
 }
