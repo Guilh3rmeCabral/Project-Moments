3570b128d654e68cfe6920d9e07c7df9843a77a9
Guilherme Cabral <guicabralprowork@gmail.com>
2023-05-02 17:57:27 -0300

    Feat: Add comentário no back e front

--- src/app/Comment.ts
+++ src/app/Comment.ts
@@ -2,7 +2,7 @@ export interface Comment {
     id?: string;
     text: string;
     username: string;
-    momentId: string;
+    momentId: number;
     created_at?: string;
     updated_at?: string;
 }
